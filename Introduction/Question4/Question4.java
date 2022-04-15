257d7d52837031ee8ab960542800351445fab46a
R G <42408254+180031240@users.noreply.github.com>
2022-04-15 12:46:51 +0530

    Update Question4.java
    Added formula

--- Introduction/Question4/Question4.java
+++ Introduction/Question4/Question4.java
@@ -7,5 +7,8 @@ package Introduction.Question4;
 public class Question4 {
 	public static void main(String[] args) {
 		System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+                int N=9;
+		System.out.println(N*(N+1)/2);
+
 	}
 }
